e76073ad26e879a3689541d3a12bdf4ca2d45f97
Otávio Odenik <otavio.odenik@gmail.com>
2025-02-12 09:13:57 -0300

    adds favoritos requests and service

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
 const rotaLivro = require('./rotas/livro');
+const cors = require("cors");
 
 app.use(express.json());
+app.use(cors({origin: "*"}));
 
 app.use('/livros', rotaLivro);
 
